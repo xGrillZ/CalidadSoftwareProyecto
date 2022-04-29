c52e196d5f855f71d66119e2a129f83bae8c8ad2
Steven Vargas <stevenalonso99@hotmail.com>
2022-04-28 21:01:59 -0600

    Proyecto general
    Proyecto general

---
+++ Calculadora/Calculadora/Calculadora.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Calculadora
+    {
+        public static int suma(int a, int b)
+        {
+            return a + b;
+        }
+        public static int resta(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int multi(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int div(int a, int b)
+        {
+            if (b == 0)
+            {
+                return -1;
+            }
+            return a / b;
+        }
+    }
+}
---
+++ Calculadora/TestCalculadora/CalculadoraTest.cs
@@ -0,0 +1,30 @@
+﻿using Calculadora;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCalculadora
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+
+        [DataRow(4, 4, 8)]
+        [DataRow(4, 0, 4)]
+        [DataRow(0, 0, 0)]
+        [TestMethod]
+        public void sumaTest(int a, int b, int esperado)
+        {
+            //Arrange
+            /*int a = 4;
+            int b = 4;
+            int esperado = 8;*/
+
+            //Act
+            int actual = Calculadora.Calculadora.suma(a, b);
+
+            //Assert
+            Assert.AreEqual(esperado,actual);
+
+
+        }
+    }
+}
